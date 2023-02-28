2ed89f7f7a2b2f440b66371e7be78f32d0037d28
averon <alexandre.veron@outlook.com>
2023-02-28 17:18:43 +0100

    42

--- test_4.cpp
+++ test_4.cpp
@@ -6,7 +6,7 @@
 /*   By: averon <averon@student.42Mulhouse.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 15:25:04 by averon            #+#    #+#             */
-/*   Updated: 2023/02/28 16:19:28 by averon           ###   ########.fr       */
+/*   Updated: 2023/02/28 16:53:07 by averon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,20 @@
 #include <iostream>
 #include <string>
 
-std::vector<std::string> str_to_vect(std::string str, char* sep)
+std::vector<std::string> str_to_vect(std::string str, std::string sep)
 {
 	std::vector<std::string> accept_vect;
 	char* tmp = new char[str.length() +1];
 
 	std::strcpy(tmp, str.c_str());
-	char *tok = std::strtok(tmp, sep);
+	char *tok = std::strtok(tmp, sep.data());
 
 	while (tok != NULL)
 	{
 		std::string(tok).erase(0, std::string(tok).find_first_not_of(" \t"));
    		std::string(tok).erase(std::string(tok).find_last_not_of(" \t") + 1);
 		accept_vect.push_back(std::string(tok));
-		tok = std::strtok(NULL, sep);	
+		tok = std::strtok(NULL, sep.data());	
 	}	
 
 	std::cout << "AFFICHAGE APRES PARSING:"<< std::endl;
