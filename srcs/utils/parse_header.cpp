13fadeca20dc2a53f411ada8fc35c4721277312a
sebastien <sebastien@ns1.loundor.com>
2023-03-31 13:48:48 +0200

    Some clean, and work on cgi.... biggggg shiiittt

--- srcs/utils/parse_header.cpp
+++ srcs/utils/parse_header.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parse_header.cpp                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: faventur <faventur@student.42mulhouse.fr>  +#+  +:+       +#+        */
+/*   By: stissera <stissera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/25 17:09:13 by stissera          #+#    #+#             */
-/*   Updated: 2023/03/31 13:16:29 by faventur         ###   ########.fr       */
+/*   Updated: 2023/03/31 13:48:08 by stissera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
