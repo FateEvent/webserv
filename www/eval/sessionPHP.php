9c8a243a49d6d5ed191722caebaf68c1f26b4541
loundor <loundor@gmail.com>
2023-05-03 21:55:52 +0200

    Need full test now!

--- www/eval/sessionPHP.php
+++ www/eval/sessionPHP.php
@@ -1,14 +1,7 @@
 <?php
-    //On démarre une nouvelle session
     session_start();
-    /*On utilise session_id() pour récupérer l'id de session s'il existe.
-     *Si l'id de session n'existe  pas, session_id() rnevoie une chaine
-     *de caractères vide*/
+
     $id_session = session_id();
-    if(!isset($_COOKIE['PHPSESSID']))
-    {
-        echo "set-cookie: PHPSESSID=" . $id_session .";\r\n\r\n";
-    }
 ?>
 <!DOCTYPE html>
 <html>
@@ -18,25 +11,25 @@
     </head>
     
     <body>
-        <h1>Titre principal</h1>
+        <h1>Session PHP-CGI</h1>
         <?php
             if($id_session)
 			{
-                echo 'ID de session récupéré via session_id : <br>' .$id_session. '<br>';
+                echo 'ID found! id session: ' .$id_session. '<br>';
             }
             echo '<br><br>';
             if(isset($_COOKIE['PHPSESSID']))
 			{
-                echo 'ID de session (récupéré via $_COOKIE) : <br>' .$_COOKIE['PHPSESSID'];
+                echo 'ID cookie : <br>' .$_COOKIE['PHPSESSID'];
             }
 
 			if(isset($_SESSION['test']))
 			{
-                echo 'Value of test: ' .$_SESSION['test']. '<br>';
+                echo 'The value of test in your session is: ' .$_SESSION['test']. '<br>';
 			}
 			else
 			{
-				echo 'Add in SESSION[test] -> My test should work!<br>';
+				echo 'Add a variable "test".<br>';
 				$_SESSION['test'] = 'My test should work!';
 			}
         ?>
--- www/eval/test2.py
+++ www/eval/test2.py
@@ -5,9 +5,11 @@ env = os.environ
 
 print("content-type: text/html; charset=UTF-8\r\n\r\n")
 
+print("<b>Your request body:</b></br>")
 for line in sys.stdin:
-    sys.stdout.write(line)
-	
+    sys.stdout.write(line + "<br/>")
+
+print("<br/><br/><b>Environement variable:</b></br>")
 for key, value in env.items():
-	print("<b>" + key + " : " + value + "<b/><br/>")
+	print(key + " : " + value + "</br>")
 
